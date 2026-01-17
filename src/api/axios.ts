3c4acd916d87daea5b5c89e2bcfc3b3beedafac6
Athif Tafrihan Achyar <athifr56@gmail.com>
2026-01-17 23:46:41 +0700

    update axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://carkus-backend-main.up.railway.app",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
 });
